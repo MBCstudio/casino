78764e7db179fa54cca68429f66f45488a17946e
MBCboy <cinek20032@wp.pl>
2026-05-07 07:46:56 +0200

    3 new charecters added

--- scripts/customer_spawner.gd
+++ scripts/customer_spawner.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-@export var customer_scene: PackedScene
+var normal_customer_scene = preload("res://scenes/actors/customers/normal_customer.tscn")
+var poor_customer_scene = preload("res://scenes/actors/customers/poor_customer.tscn")
+var rich_customer_scene = preload("res://scenes/actors/customers/rich_customer.tscn")
+var vip_customer_scene = preload("res://scenes/actors/customers/vip_customer.tscn")
+
 @export var spawn_x_min: int = 50
 @export var spawn_x_max: int = 150   # szerokość chodnika
 
@@ -14,8 +18,24 @@ func spawn_loop():
 		await get_tree().create_timer(randf_range(5.0, 8.5), false).timeout
 		spawn_customer()
 
+func get_random_customer_scene() -> PackedScene:
+	var roll = randf()
+	# 40% poor, 35% normal, 20% rich, 5% vip
+	if roll < 0.40:
+		return poor_customer_scene
+	elif roll < 0.75:
+		return normal_customer_scene
+	elif roll < 0.95:
+		return rich_customer_scene
+	else:
+		return vip_customer_scene
+
 func spawn_customer():
-	var customer = customer_scene.instantiate()
+	var scene_to_spawn = get_random_customer_scene()
+	if not scene_to_spawn:
+		return
+		
+	var customer = scene_to_spawn.instantiate()
 	
 	# 🔥 losuj czy z góry czy z dołu
 	var spawn_top = randi() % 2 == 0
